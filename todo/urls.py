9f8d5eb5463d16d7bc673c015e736a8520d909fe
mihau <wojcik.michal86@gmail.com>
2025-02-20 14:57:42 +0000

    Adding the option to add tasks

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import task_list
+from .views import task_list, add_task
 
 urlpatterns = [
     path('', task_list, name='task_list'),
+    path('add/', add_task, name='add_task'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
-from .models import Task
 
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
 def task_list(request):
     tasks = Task.objects.all()
     return render(request, 'todo/task_list.html', {'tasks': tasks})
+
+
+def add_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')  # or another page that lists tasks
+    else:
+        form = TaskForm()
+
+    return render(request, 'todo/add_task.html', {'form': form})
