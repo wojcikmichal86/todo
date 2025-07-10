97b34759aef401c99dff185022f489dff9c9d7ee
mihau <wojcik.michal86@gmail.com>
2025-07-10 10:19:56 +0100

    Adding rest of the CRUD

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import HelloAPI
+from .views import (
+    HelloAPI,
+    TaskListCreateAPIView,
+    TaskRetrieveUpdateDestroyAPIView,
+)
 
 urlpatterns = [
     path('api/hello/', HelloAPI.as_view()),
+    path('api/tasks/', TaskListCreateAPIView.as_view(), name='task-list-create'),
+    path('api/tasks/<int:pk>/', TaskRetrieveUpdateDestroyAPIView.as_view(), name='task-detail'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,18 @@
+from rest_framework import generics
+from .models import Task
+from .serializers import TaskSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
+# Lista wszystkich zadań i tworzenie nowego zadania
+class TaskListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+# Szczegóły jednego zadania, edycja i usuwanie
+class TaskRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
 
 class HelloAPI(APIView):
     def get(self, request):
