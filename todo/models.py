40af9262bbf2dd2e0c6fedd13a534fd51c792429
mihau <wojcik.michal86@gmail.com>
2025-07-10 11:04:08 +0100

    Correncting model title = name

--- todo/models.py
+++ todo/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Task(models.Model):
-    name = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
 
     def __str__(self):
